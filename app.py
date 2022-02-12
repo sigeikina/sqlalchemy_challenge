d7e916dd70c310fff2ab8c2214dc8da0cc5d2196
Oksana <oksana.sigeikina@gmail.com>
2022-02-13 02:29:03 +0800

    changes

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+import re
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.sql import exists  
+
+from flask import Flask, jsonify
+
+# Database Setup
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask Setup
+app = Flask(__name__)
+
+# Flask Routes
